3273ba9e87603ebcf30e7e733651dee645bb4363
Pola Eskandar <polaeskandar123@gmail.com>
2023-06-29 15:24:58 +0200

    Dev: added fontawesome icons.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/0fff1d5488.js" crossorigin="anonymous" defer></script>
     @vite('resources/css/app.css')
   </head>
   <body class="bg-gray-200"></body>
