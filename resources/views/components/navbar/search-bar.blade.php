92c4fa7a0e24cc46b80a8163d8000b10d0de1e36
Pola Eskandar <polaeskandar123@gmail.com>
2023-06-30 12:16:15 +0200

    Feature: added search icon to the search bar.

--- resources/views/components/navbar/search-bar.blade.php
+++ resources/views/components/navbar/search-bar.blade.php
@@ -1,6 +1,9 @@
 <div class="flex flex-col items-center w-4/6 my-10 mx-auto">
-  <form action="#" class="bg-white w-full p-3 rounded-full shadow">
-    <label for="searchbar" class="sr-only">Search something...</label>
-    <input id="searchbar" type="text" class="w-full outline-0 px-6 py-4" placeholder="Search for something..." />
+  <form action="#" class="flex items-center justify-between bg-white w-full p-3 rounded-full overflow-hidden shadow">
+    <div class="search-input w-full">
+      <label for="searchbar" class="sr-only">Search something...</label>
+      <input id="searchbar" type="text" class="w-full outline-0 px-6 py-4" placeholder="Search for something..."/>
+    </div>
+    <i class="fa-solid fa-magnifying-glass mr-6 text-gray-400 hover:text-rose-600 cursor-pointer"></i>
   </form>
 </div>
