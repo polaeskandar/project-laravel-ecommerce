a7fea02d87ee789c2dc5b8888196ed61b5ede746
Pola Eskandar <polaeskandar123@gmail.com>
2023-07-01 00:48:14 +0200

    Refactor: refactored searchbar php component.

--- app/View/Components/Searchbar/Searchbar.php
+++ app/View/Components/Searchbar/Searchbar.php
@@ -8,19 +8,6 @@ use Illuminate\View\Component;
 
 class Searchbar extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Get the view / contents that represent the component.
-     */
-    public function render(): View|Closure|string
-    {
-        return view('components.searchbar.searchbar');
-    }
+  public function __construct() { }
+  public function render(): View|Closure|string { return view('components.searchbar.searchbar'); }
 }
